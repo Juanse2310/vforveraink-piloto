2b3d916a0d662b3c227a4108eb8b60edb489f085
Juanse Giannattasio <juanse.giannattasio@gmail.com>
2022-07-23 02:40:49 -0300

    se agrega interacion con formulario de contacto

--- js/main.js
+++ js/main.js
@@ -11,3 +11,35 @@ boton.onclick = () => {
     sessionStorage.ANCHO = anch1;
     sessionStorage.ALTURA = altu2;
 };
+
+class cliente{
+    constructor(nombre,correo,mesaje){
+        this.nombre = nombre;
+        this.correo = correo;
+        this.mesaje = mesaje;
+        }
+}
+
+let boton2 = document.getElementById("enviarInfo");
+boton2.addEventListener("click", cargarCliente);
+
+function cargarCliente(){
+    let nombre = document.getElementById("nombrecito").value;
+    let correo = document.getElementById("mailcito").value;
+    let mesaje = document.getElementById("mensajito").value;
+    let cliente1 = new cliente(nombre, correo, mesaje);
+    console.log(cliente1);
+    mostrarCliente(cliente1);
+
+    function mostrarCliente(cliente){
+        let formulario = document.getElementById("customer");
+        formulario.innerHTML = "";
+
+        let nuevoContenido = document.createElement("div");
+        nuevoContenido.innerHTML = "<p> gracias"
+
+        nuevoContenido.className = "col-lg-6";
+        formulario.appendChild(nuevoContenido);
+    
+    }
+}
